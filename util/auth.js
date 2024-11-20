9cac377becc2581aefca04f978f2e1842081fa4b
sothulthorn <thornsothul@gmail.com>
2024-11-20 21:00:49 +0900

    update scret key

--- util/auth.js
+++ util/auth.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const API_KEY = 'AIzaSyAhiFwOc9seN5X7k9d4UvSfkI1ID7XBSAs';
+const API_KEY = process.env.EXPO_PUBLIC_API_KEY;
 
 export async function createUser(email, password) {
   const response = await axios.post(
-    'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY,
+    `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
     {
       email: email,
       password: password,
